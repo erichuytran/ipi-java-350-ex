0d6c6001bf9a9f90d488bd9450483ea7928b3d20
erichuytran <erictran@live.fr>
2021-02-23 14:15:23 +0100

    Added testCalculPerformanceCommercialIntegration() in EmployeServiceIntegrationTest.java

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
@@ -10,6 +10,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -46,9 +48,36 @@ class EmployeServiceIntegrationTest {
         Assertions.assertThat(employe.getMatricule()).isEqualTo("T00001");
     }
 
+    @ParameterizedTest(name = "caTraite {0}, performanceCommercial {1}, performanceExpected {2}")
+    @CsvSource({
+            " 50, 2, 1", // Si le chiffre d'affaire est inférieur de plus de 20% à l'objectif fixé, le commercial retombe à la performance de base
+            " 90, 2, 1", // Si le chiffre d'affaire est inférieur entre 20% et 5% par rapport à l'ojectif fixé, il perd 2 de performance (dans la limite de la performance de base)
+            " 101, 4, 4", // Si le chiffre d'affaire est entre -5% et +5% de l'objectif fixé, la performance reste la même.
+            " 110, 2, 4", // Si le chiffre d'affaire est supérieur entre 5 et 20%, il gagne 1 de performance (+ 1 de performance puisqu'au dessus de la moyenne [100])
+            " 150, 5, 10" // Si le chiffre d'affaire est supérieur de plus de 20%, il gagne 4 de performance (+ 1 de performance puisqu'au dessus de la moyenne [100])
+    })
+    void testCalculPerformanceCommercialIntegration(Long caTraite, Integer performanceCommercial , Integer performanceExpected) throws EmployeException {
+        Long caObjectif = 100L;
+
+        //Je ne comprends pas pourquoi cette solution ne passe pas tous les tests
+        /*
+        employeService.embaucheEmploye("Doe", "John", Poste.COMMERCIAL, NiveauEtude.LICENCE, 1.0);
+        Employe commercial = employeRepository.findAll().get(0);
+        commercial.setPerformance(performanceCommercial);
+        */
+
+        String matricule = "C12345";
+        employeRepository.save(new Employe("Doe", "John", matricule, LocalDate.now(), 1500d, performanceCommercial, 1.0));
+
+        employeService.calculPerformanceCommercial(matricule, caTraite, caObjectif);
+
+        Employe commercial = employeRepository.findByMatricule(matricule);
+        Assertions.assertThat(commercial.getPerformance()).isEqualTo(performanceExpected);
+    }
+
     @BeforeEach
     @AfterEach
-    public void purgeBDD() {
+    void purgeBDD() {
         employeRepository.deleteAll();
     }
 
