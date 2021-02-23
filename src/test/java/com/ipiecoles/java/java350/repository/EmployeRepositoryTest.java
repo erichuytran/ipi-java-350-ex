e4d115b0ac38235d8d95e3f979fbf1de7c720e3a
erichuytran <erictran@live.fr>
2021-02-23 14:31:43 +0100

    Added testAvgPerformanceWhereMatriculeStartsWith{Technicien / Commercial / Manager}() in EmployeRepositoryTest.java

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -57,6 +57,51 @@ class EmployeRepositoryTest {
         Assertions.assertThat(lastMatricule).isEqualTo("40325");
     }
 
+    @Test
+    void testAvgPerformanceWhereMatriculeStartsWithTechnicien() {
+        Integer perfTech1 = 2;
+        Integer perfTech2 = 5;
+        Integer perfTech3 = 8;
+        Double avgPerfExpected = (perfTech1 + perfTech2+ perfTech3) / 3d;
+        employeRepository.save(new Employe("Doe1", "John1", "T00001", LocalDate.now(), 1500d, perfTech1, 1.0));
+        employeRepository.save(new Employe("Doe2", "John2", "T00002", LocalDate.now(), 1500d, perfTech2, 1.0));
+        employeRepository.save(new Employe("Doe3", "John3", "T00003", LocalDate.now(), 1500d, perfTech3, 1.0));
+
+        Double avgPerf = employeRepository.avgPerformanceWhereMatriculeStartsWith("T");
+
+        Assertions.assertThat(avgPerf).isEqualTo(avgPerfExpected);
+    }
+
+    @Test
+    void testAvgPerformanceWhereMatriculeStartsWithCommercial() {
+        Integer perfComm1 = 4;
+        Integer perfComm2 = 2;
+        Integer perfComm3 = 1;
+        Double avgPerfExpected = (perfComm1 + perfComm2+ perfComm3) / 3d;
+        employeRepository.save(new Employe("Doe1", "John1", "C00001", LocalDate.now(), 1500d, perfComm1, 1.0));
+        employeRepository.save(new Employe("Doe2", "John2", "C00002", LocalDate.now(), 1500d, perfComm2, 1.0));
+        employeRepository.save(new Employe("Doe3", "John3", "C00003", LocalDate.now(), 1500d, perfComm3, 1.0));
+
+        Double avgPerf = employeRepository.avgPerformanceWhereMatriculeStartsWith("C");
+
+        Assertions.assertThat(avgPerf).isEqualTo(avgPerfExpected);
+    }
+
+    @Test
+    void testAvgPerformanceWhereMatriculeStartsWithManager() {
+        Integer perfMana1 = 8;
+        Integer perfMana2 = 7;
+        Integer perfMana3 = 7;
+        Double avgPerfExpected = (perfMana1 + perfMana2+ perfMana3) / 3d;
+        employeRepository.save(new Employe("Doe1", "John1", "M00001", LocalDate.now(), 1500d, perfMana1, 1.0));
+        employeRepository.save(new Employe("Doe2", "John2", "M00002", LocalDate.now(), 1500d, perfMana2, 1.0));
+        employeRepository.save(new Employe("Doe3", "John3", "M00003", LocalDate.now(), 1500d, perfMana3, 1.0));
+
+        Double avgPerf = employeRepository.avgPerformanceWhereMatriculeStartsWith("M");
+
+        Assertions.assertThat(avgPerf).isEqualTo(avgPerfExpected);
+    }
+
     @BeforeEach
     @AfterEach
     public void purgeBDD() {
